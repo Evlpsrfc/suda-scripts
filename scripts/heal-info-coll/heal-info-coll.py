788945a869e4da8d486f9b75fb38d8aa3fa4ac0c
evlpsrfc <evlpsrfc@163.com>
2020-09-01 15:31:41 +0800

    update head-info-coll

--- scripts/heal-info-coll/heal-info-coll.py
+++ scripts/heal-info-coll/heal-info-coll.py
@@ -1,24 +1,18 @@
 import requests
-import hashlib
 import datetime
 import re
 import json
 
 student_id = input('学号：')
 password = input('密码：')
-time = None
-# time = "00:00"
-date = None
-# date = "2049-01-01"
+time = input('时间（格式为00:00，不填默认为当前时间）：')
+date = input('日期（格式为1970-01-01，不填默认为当前日期）：')
 
+# 师生网上事务中心（sswsswzx）
+default_sswsswzx2 = "http://myauth.suda.edu.cn/default.aspx?app=sswsswzx2"
+login_jsp = "http://aff.suda.edu.cn/_web/fusionportal/login.jsp"
+login_sswsswzx2 = "http://myauth.suda.edu.cn/middleware/login?app=sswsswzx2"
 
-login_data = {
-    "IDToken1": student_id,
-    "IDToken9": password,
-    "IDToken2": hashlib.md5(password.encode("utf8")).hexdigest()
-}
-
-auth_url = "http://myauth.suda.edu.cn/default.aspx?app=sswsswzx2"
 # Health information reporting 健康信息填报（jkxxtb）
 hir_url = "http://dk.suda.edu.cn/default/work/suda/jkxxtb"
 mis_url = f"http://aff.suda.edu.cn/mobile/identityServer?targetUrl={hir_url}/jkxxcj.jsp"
@@ -80,13 +74,67 @@ def query_data(cookie_jar, time=None, date=None):
     return json.dumps({'entity': entity})
 
 
+def parse_token(html):
+    login_data = {
+        "username": student_id,
+        "password": password,
+        "source": "cas",
+        "_eventId": "submit"
+    }
+    match = re.search("name=\"execution\" value=\"(.*?)\"", html)
+    login_data["execution"] = match[1]
+    match = re.search("name=\"lt\" value=\"(.*?)\"", html)
+    login_data["lt"] = match[1]
+    return login_data
+
+
 if __name__ == "__main__":
-    r = requests.get(auth_url)
-    r = requests.post(r.url, data=login_data, allow_redirects=False)
-    if 'iPlanetDirectoryPro' not in r.cookies:
-        print('Wrong password!')
+    cookie_jar = dict()
+    r = requests.get(default_sswsswzx2)
+    # JSESSIONID
+    cookie_jar.update(r.cookies)
+    r = requests.post(r.url,
+                      data=parse_token(r.text),
+                      cookies=r.cookies,
+                      allow_redirects=False)
+    
+    if "CAS_TICKET" not in r.cookies:
+        print("账号或密码错误")
         exit(1)
-    r = requests.get(mis_url, cookies=r.cookies)
+    
+    # CAS_TICKET, CASTGC
+    cookie_jar.update(r.cookies)
+    r = requests.get(r.headers['Location'],
+                     cookies={
+                         "JSESSIONID": cookie_jar["JSESSIONID"],
+                         "CAS_TICKET": cookie_jar["CAS_TICKET"]
+                     },
+                     allow_redirects=False)
+    # JSESSIONID, LOGIN_TOKEN
+    cookie_jar.update(r.cookies)
+
+    r = requests.get(login_jsp, allow_redirects=False)
+    JSESSIONID = r.cookies["JSESSIONID"]
+    r = requests.get(login_sswsswzx2, allow_redirects=False)
+    r = requests.get(r.headers["Location"],
+                     cookies={
+                         "JSESSIONID": cookie_jar["JSESSIONID"],
+                         "LOGIN_TOKEN": cookie_jar["LOGIN_TOKEN"],
+                         "CAS_TICKET": cookie_jar["CAS_TICKET"]
+                     },
+                     allow_redirects=False)
+    r = requests.get(r.headers["Location"],
+                     cookies={
+                         "CAS_TICKET": cookie_jar["CAS_TICKET"]
+                     },
+                     allow_redirects=False)
+    r = requests.get(r.headers["Location"],
+                     cookies={
+                         "JSESSIONID": JSESSIONID
+                     },
+                     allow_redirects=False)
+    r = requests.get(mis_url, cookies={"JSESSIONID": JSESSIONID})
+
     r = requests.post(
         post_url,
         data=query_data(r.cookies, time, date),
